2cf55cd94c6203ec3182ea74c3aaa0ce18026c99
Anderson Fernandes <kabulahomestudio@gmail.com>
2024-10-23 12:40:27 -0300

    novas mudanças.

---
+++ ENEMScore.Client/wwwroot/Data/Data.cs
@@ -0,0 +1,18 @@
+﻿namespace ENEMScore.Client.Data
+{
+    public class CursoInfo
+    {
+        public string Universidade { get; set; }
+        public int Ano { get; set; }
+        public string Campus { get; set; }
+        public string Curso { get; set; }
+        public string Turno { get; set; }
+        public string Tipo { get; set; }
+        public string NotaDeCorte { get; set; }
+        public decimal CienciasDaNatureza { get; set; }
+        public decimal CienciasHumanas { get; set; }
+        public decimal Linguagens { get; set; }
+        public decimal Matematica { get; set; }
+    }
+}
+
