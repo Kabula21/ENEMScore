163d21a215377f61b0959b30fa5108e2c85db4a9
Anderson Fernandes <kabulahomestudio@gmail.com>
2024-10-25 19:53:44 -0300

    mudando tudo novamente

---
+++ ENEMScore.Client/Data/Data.cs
@@ -0,0 +1,34 @@
+﻿namespace ENEMScore.Client.Data
+{
+    public class CursoInfo
+    {
+        // Propriedade para a universidade
+        public string Universidade { get; set; }
+
+        // Propriedade para o ano
+        public int Ano { get; set; }
+
+        // Propriedade para o campus
+        public string Campus { get; set; }
+
+        // Propriedade para o curso
+        public string Curso { get; set; }
+
+        // Propriedade para o turno
+        public string Turno { get; set; }
+
+        // Propriedade para o tipo de vaga
+        public string Tipo { get; set; }
+
+        // Nota de corte do curso
+        public double NotaDeCorte { get; set; }
+
+        // Pesos das notas do ENEM
+        public double CienciasDaNatureza { get; set; }
+        public double CienciasHumanas { get; set; }
+        public double Linguagens { get; set; }
+        public double Matematica { get; set; }
+
+
+    }
+}
